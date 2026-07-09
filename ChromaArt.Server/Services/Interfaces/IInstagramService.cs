122e3cd03aa1942e68f8bbab27297c1f0ee4ce3c
Bohdan Shcherbak <68709795+BogdanSch@users.noreply.github.com>
2026-07-09 20:23:37 +0300

    Save IInstagramService.cs.

--- ChromaArt.Server/Services/Interfaces/IInstagramService.cs
+++ ChromaArt.Server/Services/Interfaces/IInstagramService.cs
@@ -1,9 +1,9 @@
-﻿using ChromaArt.Server.Dtos;
+using ChromaArt.Server.Dtos;
 
 namespace ChromaArt.Server.Services.Interfaces;
 
 public interface IInstagramService
 {
-    Task<PostDto[]> GetPostsAsync();
+    Task<PostDto[]> GetPostsAsync(Query query);
     Task<PostDto[]> FilterPosts(PostDto[] posts, string[] hashtags);
 }
