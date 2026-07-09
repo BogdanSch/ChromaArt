e59a77cf7909fd8a403c5f67fb31b91ffafef945
Bohdan Shcherbak <68709795+BogdanSch@users.noreply.github.com>
2026-07-09 20:23:43 +0300

    Save InstagramController.cs.

--- ChromaArt.Server/Controllers/InstagramController.cs
+++ ChromaArt.Server/Controllers/InstagramController.cs
@@ -1,16 +1,21 @@
-﻿using ChromaArt.Server.Services.Interfaces;
+using ChromaArt.Server.Services.Interfaces;
+using ChromaArt.Server.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChromaArt.Server.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/instagram-posts")]
 public class InstagramController(IInstagramService instagramService) : ControllerBase
 {
     private readonly IInstagramService _instagramService = instagramService;
+    private const int CACHING_DURATION_IN_SECONDS = 360;
+    public const string[] HASH_TAGS = {"digitalart"};
     [HttpGet]
-    public async Task<IActionResult> Get()
+    [ResponseCache(Duration = CACHING_DURATION_IN_SECONDS, Location = ResponseCacheLocation.Any, VaryByQueryKeys = ["page"])]
+    public async Task<IActionResult> Get(Query query)
     {
-        return Ok(await _instagramService.GetPostsAsync());
+        PostDto[] posts = await _instagramService.GetPostsAsync();
+        return Ok(_instagramService.FilterPosts(posts, HASH_TAGS));
     }
 }
